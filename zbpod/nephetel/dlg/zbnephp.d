0f34d96d0593205553b225214734c6a3fd39724a
dark0dave <dark0dave@mykolab.com>
2026-04-12 16:48:50 +0100

    fix(straightoutofthepits): updated dialogue for straight out the pits
    Signed-off-by: dark0dave <dark0dave@mykolab.com>

--- zbpod/nephetel/dlg/zbnephp.d
+++ zbpod/nephetel/dlg/zbnephp.d
@@ -3,24 +3,41 @@ BEGIN ZBNEPHP
 // Kickout Dialogue
 IF ~Global("ZB_NEPH_JOINS","GLOBAL",1) Global("ZB_NEPH_LEFT","GLOBAL",0)~ ZBNEPHKICKOUT
   SAY @116 /* You wish to part ways? */
-  IF ~~ THEN REPLY @117 /* Yes, Nephetel. We cannot continue together right now. Please meet us back at the Copper Coronet if you wish to join forces once again. */ DO ~
+  IF ~~ THEN REPLY @117 /* Indeed, we can’t travel on as we have for now. Go to the Copper Coronet so I know where to find you if I need you once more. */ DO ~
     SetGlobal("ZB_NEPH_MOVE_COPPER","GLOBAL",1)
     SetGlobal("ZB_NEPH_ESCAPE_1","GLOBAL",4)
     SetGlobal("ZB_NEPH_LEFT","GLOBAL",1)
     StartCutSceneMode()
     StartCutScene("zbnephm")~ EXIT
+  IF ~Global("ShadowWork","GLOBAL",1)~ THEN REPLY @370 /* For now, yes. Head to the Shadow Thief guild in the docks. I’ll meet you there should I require your help again. */ DO ~
+    SetGlobal("ZB_NEPH_MOVE_SHADOW_THIEF","GLOBAL",1)
+    SetGlobal("ZB_NEPH_ESCAPE_1","GLOBAL",4)
+    SetGlobal("ZB_NEPH_LEFT","GLOBAL",1)
+    StartCutSceneMode()
+    StartCutScene("zbnephmt")~ EXIT
   IF ~~ THEN REPLY @118 /* I don't know what I was thinking, of course I want you to stay. */ EXIT
 END
 
 // Rejoin
 IF ~Global("ZB_NEPH_JOINS","GLOBAL",1) Global("ZB_NEPH_LEFT","GLOBAL",1)~ ZBNEPHREJOIN
-  SAY @119 /*  So the prodigal bhaalspawn returns. I assume you have changed your mind and wish to continue our journey together? */
+  SAY @119 /* So, the prodigal Bhaalspawn returns. I assume that you’ve changed your mind and wish to continue our journey together? */
   IF ~~ THEN REPLY @120 /* Yes, please join us. */ DO ~SetGlobal("ZB_NEPH_LEFT","GLOBAL",0)~ GOTO ZBNEPHJOIND
   IF ~~ THEN REPLY @121 /* Not right now. We will contact you if we have a need for you. */ EXIT
 END
 
 // Join
 IF ~~ ZBNEPHJOIND
-  SAY @122 /* I'm glad you regained your senses, lead the way. */
-  IF ~~ DO ~SetGlobal("ZB_NEPH_LEFT","GLOBAL",0) JoinParty()~ EXIT
+  SAY @122 /* Good! I’d be surprised if you thought my talents truly were of no use to you. Let’s get going. */
+  IF ~~ DO ~SetGlobal("ZB_NEPH_LEFT","GLOBAL",0) MakeGlobal() JoinParty()~ EXIT
+END
+
+// TODO: Trigger this due to ?
+// Leave Due to Reputation
+IF ~False()~ ZBNEPHLEAVEICANTSTANDSIGHTOFYOU
+  SAY @371 /* I can’t stand the sight of you, <CHARNAME>. I hope we never meet again. */
+  IF ~~ DO ~
+    SetGlobal("ZB_NEPH_REP_LEFT","GLOBAL",1)
+    SetGlobal("ZB_NEPH_LEFT","GLOBAL",1)
+    StartCutSceneMode()
+    StartCutScene("zbnephm")~ EXIT
 END
